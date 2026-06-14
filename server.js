210f1166b8bd81528daee142185ae43220289992
czluro <nguyenminhhaiduc@gmail.com>
2026-06-14 14:07:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ app.get('/live/:username', (req, res) => {
     const username = req.params.username;
     const tiktokUrl = `https://www.tiktok.com/@${username}/live`;
 
-    const command = `yt-dlp -g "${tiktokUrl}"`;
+    // Nhập cái IP:PORT Việt Nam ông vừa tìm được vào đây (chú ý định dạng http hoặc socks5)
+    const proxyVN = "http://115.74.159.232:1080"; 
+
+    // Thêm cờ --proxy vào lệnh yt-dlp
+    const command = `yt-dlp --proxy "${proxyVN}" -g "${tiktokUrl}"`;
+
+    exec(command, (error, stdout, stderr) => {
+        // ... (phần code bên dưới giữ nguyên) ...
 
     exec(command, (error, stdout, stderr) => {
         if (error || !stdout) {
